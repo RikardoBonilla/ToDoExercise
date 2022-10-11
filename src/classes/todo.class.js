2f68ea0fafa8c86bc41f559d761416ae24612769
Ricardo <ricardoandresbonilla@gmail.com>
2022-10-10 19:23:41 -0500

    Primer commit

---
+++ src/classes/todo.class.js
@@ -0,0 +1,25 @@
+export class Todo {
+
+    static fromJson( { id, tarea, completado, creado } ){
+        const tmpTodo = new Todo( tarea );
+        
+        tmpTodo.id          = id;
+        tmpTodo.completado  = completado;
+        tmpTodo.creado      = creado;
+
+        return tmpTodo;
+
+    }
+
+    constructor( tarea ){
+        this.tarea  = tarea;
+        
+        this.id     = new Date().getTime();
+        this.completado = false;
+        this.creado = new Date();
+    }
+    
+    imprimirClase(){
+        console.log(`${ this.tarea } - ${ this.id } `);
+    }
+}
